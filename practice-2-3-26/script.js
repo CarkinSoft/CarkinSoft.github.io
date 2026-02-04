eba8dd3eb5c35408fd561ed57190cd05a1591ad2
CarkinOtter <carcardoza@csumb.edu>
2026-02-03 16:32:02 -0800

    :monocle_face: moved folders around and organized them a little better

---
+++ practice-2-3-26/script.js
@@ -0,0 +1,18 @@
+
+/*
+DONT USE VAR, unsafe
+var a = 1;
+
+USE LET for variables
+let b = 2;
+
+Constant cannot change
+const c = 3;
+
+Variable names cannot start with a number and should not include spaces or operators.
+JS is a loosely typed language. Varaiblaes can be assigned different types.
+*/
+
+let today = new Date();
+console.log(today);
+alert(today);
